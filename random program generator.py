5240ab4b734a6d80806d4d97c530323a7e6468c2
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2022-10-01 18:02:18 -0400

    format code

--- random program generator.py
+++ random program generator.py
@@ -1,7 +1,9 @@
 import random
 import time
 
-#interpreter to run the code, I have no clue how it works
+# interpreter to run the code, I have no clue how it works
+
+
 def interpret(code):
     try:
         array = [0]
@@ -10,37 +12,37 @@ def interpret(code):
         i = 0
         result = ''
         t1 = time.time()
-        t= time.time()-t1
+        t = time.time()-t1
         while i < len(code) and t < 1:
             if code[i] == '<':
                 if pointerLocation > 0:
                     pointerLocation -= 1
                     t = time.time() - t1
                     if t > 0.5:
-                        return [result,code,t]
+                        return [result, code, t]
             elif code[i] == '>':
                 pointerLocation += 1
                 t = time.time() - t1
                 if t > 0.5:
-                        return [result,code,t]
+                    return [result, code, t]
                 if len(array) <= pointerLocation:
                     array.append(0)
             elif code[i] == '+':
                 array[pointerLocation] += 1
                 t = time.time() - t1
                 if t > 0.5:
-                        return [result,code,t]
+                    return [result, code, t]
             elif code[i] == '-':
                 if array[pointerLocation] > 0:
                     array[pointerLocation] -= 1
                 t = time.time() - t1
                 if t > 0.5:
-                        return [result,code,t]
+                    return [result, code, t]
             elif code[i] == '.':
                 result += chr(array[pointerLocation])
                 t = time.time() - t1
                 if t > 0.5:
-                        return [result,code,t]
+                    return [result, code, t]
             elif code[i] == ',':
                #x = input("Input (1 CHARACTER!):")
                 x = '0'
@@ -51,102 +53,113 @@ def interpret(code):
                 array[pointerLocation] = y
                 t = time.time() - t1
                 if t > 0.5:
-                    return [result,code,t]
+                    return [result, code, t]
             elif code[i] == '[':
                 if array[pointerLocation] == 0:
                     open_braces = 1
                     while open_braces > 0:
                         t = time.time() - t1
                         if t > 0.5:
-                            return [result,code,t]
+                            return [result, code, t]
                         i += 1
                         if code[i] == '[':
                             open_braces += 1
                             t = time.time() - t1
                             if t > 0.5:
-                                return [result,code,t]
+                                return [result, code, t]
                         elif code[i] == ']':
                             open_braces -= 1
                             t = time.time() - t1
                             if t > 0.5:
-                                return [result,code,t]
+                                return [result, code, t]
             elif code[i] == ']':
                 # you don't need to check array[pointerLocation] because the matching '[' will skip behind this instruction if array[pointerLocation] is zero
                 open_braces = 1
                 while open_braces > 0:
                     t = time.time() - t1
                     if t > 0.5:
-                        return [result,code,t]
+                        return [result, code, t]
                     i -= 1
                     if code[i] == '[':
                         open_braces -= 1
                         t = time.time() - t1
                         if t > 0.5:
-                            return [result,code,t]
+                            return [result, code, t]
                     elif code[i] == ']':
                         open_braces += 1
                         t = time.time() - t1
                         if t > 0.5:
-                            return [result,code,t]
+                            return [result, code, t]
                 # i still gets incremented in your main while loop
                 i -= 1
-            t= time.time()-t1
+            t = time.time()-t1
             if t > 0.5:
-                return [result,code,t]
+                return [result, code, t]
             i += 1
-        t= time.time()-t1
-        return [result[0:2],code,t]
+        t = time.time()-t1
+        return [result, code, t]
     except:
-            return ['failure:(',code,5]
+        return ['failure:(', code, 5]
 
-#create a program
+# create a program
+
+
 def generaterandom():
     generator = ""
-    #find the length of the program
-    for x in range(random.randint(1,100)):
-        #pick a random character to add
-        generator += random.choice(("<",">","+","-","[","]",".",","))
+    # find the length of the program
+    for x in range(random.randint(1, 100)):
+        # pick a random character to add
+        generator += random.choice(("<", ">", "+", "-", "[", "]", ".", ","))
     return generator
+
+
 NextGen = []
 results = []
 Solved = False
 m = 0
 
-#randomly edit the program
+# randomly edit the program
+
+
 def modify(code):
     try:
         workspace = [x for x in code]
-        for x in range(random.randint(0,10)):
-            z = random.randint(0,len(workspace)-1)
-            select = random.randint(1,3)
-            #remove a character
+        for x in range(random.randint(0, 10)):
+            z = random.randint(0, len(workspace)-1)
+            select = random.randint(1, 3)
+            # remove a character
             if select == 1:
                 workspace.pop(z)
-            
-            #modify a character
+
+            # modify a character
             elif select == 2:
-                workspace[z] = random.choice(("<",">","+","-","s[","]s",".",","))
-            
-            #insert a charcter
+                workspace[z] = random.choice(
+                    ("<", ">", "+", "-", "s[", "]s", ".", ","))
+
+            # insert a charcter
             elif select == 3:
-                workspace.insert(z,random.choice(("<",">","+","-","[","]",".",",")))
-            #splice a piece of code
-            #elif select == 4:
+                workspace.insert(z, random.choice(
+                    ("<", ">", "+", "-", "[", "]", ".", ",")))
+            # splice a piece of code
+            # elif select == 4:
                 #chunks = code2[0].split('s')[0]
-                #workspace.insert(z,[random.randint(0,len(chunks))-1])
+                # workspace.insert(z,[random.randint(0,len(chunks))-1])
         return ''.join(workspace)
     except:
         return ''
 
-#create a list with 100 objects
+
+# create a list with 100 objects
 for repeat in range(100):
     NextGen.append(generaterandom())
 
 timer = time.time()
 
-#find the fitness of code
+# find the fitness of code
+
+
 def fitness(code):
-    fit=0
+    fit = 0
     out = code[0]
     ind = 0
     if out == "failure:(":
@@ -157,49 +170,50 @@ def fitness(code):
     fit -= code[2]/10
     for char in out:
         if char == 'hi'[ind]:
-            fit+=5
+            fit += 5
     for char2 in out:
         if char2 == 'h' or char2 == 'i':
-            fit+=3
-    fit+= abs(2-len(out))
-    if len(out)> ind+1:
-        ind+=1
-    fit-= len(code[1])/100
+            fit += 3
+    fit += abs(2-len(out))
+    if len(out) > ind+1:
+        ind += 1
+    fit -= len(code[1])/100
     return fit
+
 
 fitnesses = []
 best = []
 notfirsttime = False
-#main loop
+# main loop
 while Solved == False:
-    #output the time of the generation
+    # output the time of the generation
     print('time: ' + str(time.time()-timer))
     if notfirsttime:
         print(best[-1][0][0:2] + ' ' + str(fitnesses[-1]))
     else:
         notfirsttime = True
     m = 0
-    #run the code
+    # run the code
     for l in range(len(NextGen)):
         bfcode = NextGen[l]
-        #try running the code
+        # try running the code
         try:
             results.append(interpret(bfcode))
-        #if it fails give it a bad score
+        # if it fails give it a bad score
         except:
-            results.append(['failure:(',bfcode,5])
+            results.append(['failure:(', bfcode, 5])
     highscore = -100
     NextGen = []
     for each in range(len(results)):
         fit = fitness(results[each])
-        #find the best code
+        # find the best code
         if fit > highscore:
             highscore = fit
             best.append(results[each])
         if results[each][0][0:2] == 'hi':
-            solved =True
+            solved = True
             finalcode = results[each][1]
-            #print(finalcode)
+            # print(finalcode)
             break
         fitnesses.append(fit)
     while len(best) > 100:
